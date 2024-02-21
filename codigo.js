27e80dfaea9addfbeb271fbaf1a198e518f7b1a4
David <noe.salazar0510@gmail.com>
2024-02-20 23:50:21 -0600

    Avances Practica 4

--- codigo.js
+++ codigo.js
@@ -1,9 +1,28 @@
-function login() {
-    var usuario = document.getElementById("usuario").value;
-    document.getElementById("mensaje").innerHTML = "Login correcto. Bienvenido, " + usuario + "!";
+var productoInput = document.getElementById("productotxt");
+var costoInput = document.getElementById("preciotxt");
+
+var listaProductos = document.getElementById("listaProductos");
+var totalElement = document.getElementById("total");
+
+var total = 0;
+
+function agregar() {
+    var producto = productoInput.value;
+    var costo = parseInt(costoInput.value);
+
+    total += costo;
+
+    var newElement = document.createElement('p');
+    newElement.innerHTML = producto + ".......$" + costo;
+
+    listaProductos.appendChild(newElement);
+
+    totalElement.innerHTML = "Total: $" + total;
+
+    limpiar();
 }
 
-function registro() {
-    var usuario = document.getElementById("usuario").value;
-    document.getElementById("mensaje").innerHTML = "Registro exitoso. Bienvenido, " + usuario + "!";
+function limpiar() {
+    productoInput.value = "";
+    costoInput.value = "";   
 }
