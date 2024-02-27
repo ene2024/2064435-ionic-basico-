dfca208a3585a08c02f1cb1a9d52afdece509b8c
David <noe.salazar0510@gmail.com>
2024-02-27 17:46:08 -0600

    Practica 4

--- codigo.js
+++ codigo.js
@@ -1,23 +1,28 @@
+
 var productoInput = document.getElementById("productotxt");
 var costoInput = document.getElementById("preciotxt");
 
-var listaProductos = document.getElementById("listaProductos");
+var listaProductos = document.getElementById("LISTADO");
 var totalElement = document.getElementById("total");
 
 var total = 0;
+var producto = "";
+var costo = 0;
+
 
 function agregar() {
-    var producto = productoInput.value;
-    var costo = parseInt(costoInput.value);
+    producto = productoInput.value;
+    costo = parseInt(costoInput.value);
 
     total += costo;
 
     var newElement = document.createElement('p');
     newElement.innerHTML = producto + ".......$" + costo;
 
-    listaProductos.appendChild(newElement);
+    listaProductos.insertBefore(newElement, totalElement);
 
     totalElement.innerHTML = "Total: $" + total;
+
 
     limpiar();
 }
