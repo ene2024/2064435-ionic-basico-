992b138c8cfd57d9da8a2b2990dd667f6ef2438c
David <noe.salazar0510@gmail.com>
2024-04-04 20:05:01 -0600

    Practica8: Action Sheet y Sliding Options

--- miApp-2064435/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-2064435/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.scss'],
 })
-export class ListaAlumnosComponent  implements OnInit {
-
-  constructor() { }
+export class ListaAlumnosComponent implements OnInit {
+  constructor(
+    private alertController: AlertController,
+    private actionSheetController: ActionSheetController
+  ) {}
 
   ngOnInit() {}
+
   alumnos: any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
+
+  async addToFavorites(alumno: string) {
+    // Lógica para agregar a favoritos
+  }
+
+  async confirmDelete(alumno: string) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Eliminar',
+      buttons: [
+        {
+          text: 'Eliminar',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            // Lógica para eliminar el alumno
+          }
+        },
+        {
+          text: 'Cancelar',
+          icon: 'close',
+          role: 'cancel'
+        }
+      ]
+    });
+    await actionSheet.present();
+  }
 }
 
